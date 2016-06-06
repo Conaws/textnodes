66387e29a9fc9c3733966bb8323a66d88ac58fa4
Conor White-Sullivan <conor@Conors-MacBook-Pro.local>
2016-06-06 14:15:40 +0530

    Reducing into edge-arrays correctly

--- src/clj/text_nodes/spec-test.clj
+++ src/clj/text_nodes/spec-test.clj
@@ -15,21 +15,10 @@
                 extend-protocolpath]]))
 ;@+node:conor.20160606062933.1: *3* string spec
 ;@+node:conor.20160606062941.1: *4* sampletext
-(def sampletext "This is the first goal\n\tThis is it's first child\n\t\t:person Conor")
+(def sampletext "This is the first goal\n\tThis is it's first child\n\t\t@person Conor")
 ;@+node:conor.20160606073225.1: *4* splitting the string with types
 ;@+others
-;@+node:conor.20160606073225.2: *5* (defn count-tabs  [string]  
-(defn count-tabs
-  [string]
-  (count (take-while #{\tab} string)))
-  
-;@+node:conor.20160606075341.1: *5* using split-with
-;@+node:conor.20160606120955.1: *6* set-members-in-string
-(defn members-in-string  [trigset string]
-   (set/intersection trigset (str/split string #"\s")))
-
-
-;@+node:conor.20160606113823.1: *6* splitting text using conforms
+;@+node:conor.20160606113823.1: *5* splitting text using conforms
 
 (def trigger #{"@person" "@role"})
 
@@ -46,47 +35,44 @@
 
 (s/def ::even-parse  (s/* 
                       (s/or :edge  ::edgeparse
-                            :child (s/spec (s/* ::not-trigger)))))
-
-
-;@+others
-;@+node:conor.20160606123700.1: *7* WIN  reducing pattern volume 3
+                            :child (s/spec (s/+ ::not-trigger)))))
 
 
 
-(def newsamp ["Hey" "@person" "Conor" "White-Sullivan" "@result" "nada" "@final"])
-
+;@+node:conor.20160606123700.1: *5* vals-between
 
 (defn vals-between [resetfn s]
   (->> s
        (reduce (fn [{c :c r :r :as m} x]
                  (if (resetfn x)
-                   (assoc m :c [x] :r (conj r c))
+                   (assoc m :c [x] :r (if (empty? c)
+                                        r
+                                        (conj r c)))
                    (assoc m :c (conj c x))))
                {:c [] :r []})
-       ((fn [{c :c r :r}]  (conj r c)))))
-
-(def parsedf
-  (vals-between #(s/valid? ::trigger %) newsamp))       
-                          
-
-(pprint (s/conform ::even-parse parsedf))
-
-;@-others
+       ((fn [{c :c r :r}] (conj r c)))))
 
 
+;@+node:conor.20160606073225.2: *5* (defn count-tabs  [string]  
+(defn count-tabs
+  [string]
+  (count (take-while #{\tab} string)))
+  
 ;@+node:conor.20160606073225.3: *5* (defn parsed [text]  (->> 
+
+
+(defn check-for-edges [string]
+  (->  (str/trim string)
+       (str/split #"\s+")
+       (->> (vals-between #(s/valid? ::trigger %)))))
 
 (defn parsed [text]
   (->> (str/split text #"\n")
-       (map (juxt count-tabs str/trim))))
+       (map (juxt count-tabs check-for-edges))))
+
+
+#_(parsed sampletext)
 
 ;@-others
-;@+node:conor.20160606070500.1: *4* Test the Edgespec
-;@+node:conor.20160606070517.1: *5* returning the conformed value
-(for [[i t] (parsed sampletext)]
-    [i (s/conform ::edges t)])
-    
-    
 ;@-others
 ;@-leo
