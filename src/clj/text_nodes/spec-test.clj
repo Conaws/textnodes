b8b3d79104711ee26189500e01e0c6928b158a00
Conor White-Sullivan <conor@Conors-MacBook-Pro.local>
2016-06-06 06:44:53 +0530

    Splitting text into tab array

--- src/clj/text_nodes/spec-test.clj
+++ src/clj/text_nodes/spec-test.clj
@@ -1,10 +1,38 @@
+;@+leo-ver=5-thin
+;@+node:conor.20160606052632.1: * @file spec-test.clj
+;@+others
+;@+node:conor.20160606053224.1: ** spec-test
+;@+node:conor.20160605013418.1: *3* namespace -- datascript and specter
 (ns text-nodes.core
-  (:require 
-            [com.rpl.specter :as sp :refer [ALL]]
-            [clojure.spec :as s] 
-            [clojure.pprint :refer [pprint]]
-            [datascript.core  :as d])
-  (:use [com.rpl.specter.macros 
+  (:require [com.rpl.specter  :as sp :refer [ALL]]
+                [clojure.spec        :as s]
+                [clojure.string      :as str]
+                [clojure.pprint       :refer [pprint]]
+                [datascript.core    :as db])
+  (:use 
+   [com.rpl.specter.macros 
          :only [select transform defprotocolpath
                 extend-protocolpath]]))
+;@+node:conor.20160606062933.1: *3* string spec
+;@+node:conor.20160606062941.1: *4* sampletext
+(def sampletext "This is the first goal\n\tThis is it's first child\n\t\t:assigned-to Conor")
+;@+node:conor.20160606063153.1: *4* splitting the string with types
+;@+others
+;@+node:conor.20160606064205.1: *5* (defn count-tabs  [string]  
+(defn count-tabs
+  [string]
+  (count (take-while #{\tab} string)))
+  
+;@+node:conor.20160606064205.2: *5* (defn parsed [text]  (->> 
 
+(defn parsed [text]
+  (->> (str/split text #"\n")
+       (map (juxt count-tabs str/trim))))
+
+;@+node:conor.20160606064219.1: *5* (parsed sampletext)
+(parsed sampletext)
+
+
+;@-others
+;@-others
+;@-leo
